1cc85bda4a195a5c011981c863116d03512aca3e
Corvus278 <makar.korolenko2005@mail.ru>
2021-10-04 16:01:07 +0300

    ssr

---
+++ module_1/cfg/webpack.client.config.js
@@ -0,0 +1,26 @@
+const path = require('path')
+
+const NODE_ENV = process.env.NODE_ENV
+
+
+module.exports = {
+    mode: NODE_ENV ? NODE_ENV : 'development',
+
+    entry: path.resolve(__dirname, '../src/client/index.jsx'),
+
+    output: {
+        path: path.resolve(__dirname, '../dist/client'),
+        filename: 'client.js',
+    },
+
+    resolve: {
+        extensions: ['.js', '.json', '.css', '.ts', '.jsx']
+    },
+
+    module: {
+        rules: [{
+            test: /\.[tj]sx?$/,
+            use: ['ts-loader']
+        }],
+    },
+}
